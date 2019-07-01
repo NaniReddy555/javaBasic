d38a62b451ac5f6c4f6b3e4b1914dedc11af0d25
sravan <sravan.ba204@gmail.com>
2019-07-01 20:49:17 +0530

    new one

---
+++ program1.java
@@ -0,0 +1,14 @@
+class Arrays
+{
+public static void main(String[]args)
+{
+Object[]a=new Object[10];
+a[0]=10;
+a[1]="sravan";
+a[2]=10.5;
+a[3]=20.5f;
+a[4]='B';
+System.out.println("the output is ");
+System.out.println(a[0]+" "+a[1]+" "+a[2]+" "+a[3]+" "+a[4]);
+}
+}
